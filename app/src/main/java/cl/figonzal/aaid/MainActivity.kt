d768da3a298bf2ad27bd301da1259d864718a3ff
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2022-12-10 17:23:09 -0300

    Implementación splash screen y logo app vectorizado
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/src/main/java/cl/figonzal/aaid/MainActivity.kt
+++ app/src/main/java/cl/figonzal/aaid/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
 import cl.figonzal.aaid.ui.AAIDViewModel
 import cl.figonzal.aaid.ui.MainScreen
@@ -12,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
 
             val viewModel: AAIDViewModel = viewModel()
