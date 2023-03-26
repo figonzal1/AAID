8ecae0b2188691fe42f89b4e59c95e0dbd55ba2b
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-03-26 16:09:09 -0300

    1.2 (4) version launch
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "cl.figonzal.aaid"
         minSdk = 23
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
