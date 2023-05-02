6529603dcbd54520147b009fd3ae04e37d21a97f
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-05-01 23:30:39 -0400

    plugins improvements & non final res
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,26 +12,19 @@
  * Last modified: 13-01-23 15:28
  */
 
-buildscript {
-
-    dependencies {
-
-        //FIREBASE CRASH ANALYTICS
-        classpath("com.google.gms:google-services:4.3.15")
-
-        //FIREBASE CRASHLYTICS
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
-
-        //FIREBASE PERFORMANCE
-        classpath("com.google.firebase:perf-plugin:1.4.2")
-
-        //Google maps secrets
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
+
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+
+    id("com.google.gms.google-services") version "4.3.15" apply false
+
+    id("com.google.firebase.crashlytics") version "2.9.5" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
+
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1"
+
     id("org.sonarqube") version "4.0.0.2929"
 }
