5a7adaaf13b5607df00f56a6c2e70bec02d89789
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-03-09 17:57:25 -0300

    update dependencies & upadte fastlane
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "cl.figonzal.aaid"
         minSdk = 23
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.2.5"
+        versionCode = 10
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
