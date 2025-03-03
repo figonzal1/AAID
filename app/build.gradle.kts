823656de423fa44b69b56474e4d3483a238cfc19
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2025-03-02 22:15:58 -0300

    version 1.2.10
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "cl.figonzal.aaid"
         minSdk = 23
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.2.9"
+        versionCode = 14
+        versionName = "1.2.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
